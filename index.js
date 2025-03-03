2737620ab2896b7dcc280f751b2c82ea32543f7a
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-03 08:40:09 +0300

    updated version

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     links.forEach(link => {
         link.addEventListener("click", function (event) {
-            // Remove 'active' class from all links
+           
             links.forEach(l => l.classList.remove("active"));
 
-            // Add 'active' class to clicked link
+           
             this.classList.add("active");
         });
     });
